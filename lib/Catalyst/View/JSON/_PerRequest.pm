9cc160aeacf835f921866752a541ccf37fe36f81
John Napiorkowski <jjn1056@yahoo.com>
2015-09-20 14:54:15 -0500

    first post

---
+++ lib/Catalyst/View/JSON/_PerRequest.pm
@@ -0,0 +1,116 @@
+package Catalyst::View::JSON::_PerRequest;
+
+use HTTP::Status;
+use Scalar::Util;
+
+sub data {
+  my ($self, $data) = @_;
+  if($data) {
+    if($self->{data}) {
+      die "Can't set view data attribute if its already set";
+    } else {
+      $data = $self->{ctx}->model($data) unless ref $data;
+      die "Model $data does not do a required method 'TO_JSON'"
+        unless $data->can('TO_JSON');
+
+      return $self->{data} = $data;
+    }
+  } else {
+    return $self->{data} ||= do {
+      my $default_view_model = $self->{parent}->default_view_model;
+      $default_view_model = $self->{ctx}->model($default_view_model)
+        unless ref $default_view_model;
+      $default_view_model;
+    };
+  }
+}
+
+sub callback_param {
+  my ($self, $value) = @_;
+  $self->{callback_param} = $value;
+}
+
+sub send {    
+  my ($self, @proto) = @_;
+  my ($status, @headers) = ();
+  
+  if(ref \$proto[0] eq 'SCALAR') {
+    $status = shift @proto;
+  } else {
+    $status = 200;
+  }
+
+  if(ref $proto[$#proto] eq 'HASH') {
+    my $var = pop @proto;
+    foreach my $key (keys %$var) {
+      $self->data->$key($var->{$key});
+    }
+  }
+
+  if(Scalar::Util::blessed($proto[$#proto])) {
+    my $obj = pop @proto;
+    $self->data($obj);
+  }
+
+  if(@proto) {
+    @headers = @{$proto[0]};
+  }
+
+  $self->{ctx}->stats->profile(begin => "=> JSON->send". ($status ? "($status)": ''))
+    if $self->{ctx}->debug; 
+
+  my $res = $self->{ctx}->response;
+
+  $res->headers->push_headers(@headers) if @headers;
+  $res->status($status);
+  $res->content_type('application/json') unless $res->content_type;
+
+  my $json = $self->{json}->encode($self->data);
+
+  if(my $param = $self->{callback_param}) {
+    my $cb = $c->req->query_parameter($cbparam);
+    $cb =~ /^[a-zA-Z0-9\.\_\[\]]+$/ || die "Invalid callback parameter $cb";
+    $json = "$cb($json)";
+  }
+
+  $res->body($json) unless $res->has_body;
+}
+
+# Send Helpers.
+foreach my $helper( grep { $_=~/^http/i} @HTTP::Status::EXPORT_OK) {
+  my $subname = lc $helper;
+  $subname =~s/http_//i;  
+  eval "sub send_$subname { return shift->send(HTTP::Status::$helper,\@_) }";
+}
+
+1;
+
+=head1 NAME
+
+Catalyst::View::JSON::_PerRequest - Private object for JSON views that own data
+
+=head1 SYNOPSIS
+
+    No user servicable bits
+
+=head1 DESCRIPTION
+
+See L<Catalyst::View::JSON::PerRequest> for details.
+
+=head1 SEE ALSO
+
+L<Catalyst>, L<Catalyst::View>, L<Catalyst::View::JSON::PerRequest>,
+L<HTTP::Status>
+
+=head1 AUTHOR
+ 
+John Napiorkowski L<email:jjnapiork@cpan.org>
+  
+=head1 COPYRIGHT & LICENSE
+ 
+Copyright 2015, John Napiorkowski L<email:jjnapiork@cpan.org>
+ 
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
---
+++ t/basic.t
@@ -0,0 +1,148 @@
+use Test::Most;
+
+{
+  package MyApp::Model::Person;
+
+  use Moo;
+  extends 'Catalyst::Model';
+
+  has [qw/first_name last_name age/] => (is=>'rw');
+
+  sub TO_JSON {
+    my $self = shift;
+    return {
+      time => scalar(localtime),
+      fname => $self->first_name,
+      lname => $self->last_name,
+      age => $self->age };
+  }
+
+  sub ACCEPT_CONTEXT {
+    my ($self, $c, @args) = @_;
+    return ref($self)->new(@args);
+  }
+
+  $INC{'MyApp/Model/Person.pm'} = __FILE__;
+
+  package MyApp::View::JSON;
+
+  use Moo;
+  extends 'Catalyst::View::JSON::PerRequest';
+
+  package MyApp::Controller::Root;
+  use base 'Catalyst::Controller';
+
+  $INC{'MyApp/View/JSON.pm'} = __FILE__;
+
+  sub example :Local Args(0) {
+    my ($self, $c) = @_;
+    $c->view->send_ok({
+        a => 1,
+        b => 2,
+        c => 3,
+      });
+  }
+
+  sub custom :Local Args(0) {
+    my ($self, $c) = @_;
+    $c->view->data('Person');
+    $c->view->data->last_name('nap');
+    $c->view->data->first_name('john');
+    $c->view->send_ok({age => 44});
+  }
+
+  sub object :Local Args(0) {
+    my ($self, $c) = @_;
+    $c->view->send_ok(
+      $c->model('Person',
+        first_name => 'M', 
+        last_name => 'P',
+        age => 20));
+  }
+
+  sub root :Chained(/) CaptureArgs(0) {
+    my ($self, $c) = @_;
+    $c->view->data->set(z=>1);
+  }
+
+  sub a :Chained(root) CaptureArgs(0) {
+    my ($self, $c) = @_;
+    $c->view->data->set(y=>1);
+
+  }
+
+  sub b :Chained(a) Args(0) {
+    my ($self, $c) = @_;
+    $c->view->send_created({
+        a => 1,
+        b => 2,
+        c => 3,
+      });
+  }
+
+  $INC{'MyApp/Controller/Root.pm'} = __FILE__;
+
+  package MyApp;
+  
+  use Catalyst;
+
+  MyApp->config(
+    default_view =>'JSON',
+    'Controller::Root' => { namespace => '' },
+  );
+
+  MyApp->setup;
+}
+
+use Catalyst::Test 'MyApp';
+use JSON::MaybeXS;
+
+{
+  ok my ($res, $c) = ctx_request( '/example' );
+  is $res->code, 200;
+  
+  my %json = %{ decode_json $res->content };
+
+  is $json{a}, 1;
+  is $json{b}, 2;
+  is $json{c}, 3;
+}
+
+{
+  ok my ($res, $c) = ctx_request( '/root/a/b' );
+  is $res->code, 201;
+  
+  my %json = %{ decode_json $res->content };
+
+  is $json{a}, 1;
+  is $json{b}, 2;
+  is $json{c}, 3;
+  is $json{y}, 1;
+  is $json{z}, 1;
+}
+
+{
+  ok my ($res, $c) = ctx_request( '/custom' );
+  is $res->code, 200;
+  
+  my %json = %{ decode_json $res->content };
+
+  is $json{fname}, 'john';
+  is $json{lname}, 'nap';
+  is $json{age}, 44;
+  ok $json{time};
+}
+
+{
+  ok my ($res, $c) = ctx_request( '/object' );
+  is $res->code, 200;
+  
+  my %json = %{ decode_json $res->content };
+
+  is $json{fname}, 'M';
+  is $json{lname}, 'P';
+  is $json{age}, 20;
+  ok $json{time};
+}
+
+done_testing;
